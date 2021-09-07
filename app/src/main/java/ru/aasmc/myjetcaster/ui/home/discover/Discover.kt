473cc5dc0661db23a0d60cefb29d238a4d697c8e
Alex Alexeyev <aasmc@yandex.ru>
2021-09-07 22:58:57 +0300

    Implement Discover.kt

--- app/src/main/java/ru/aasmc/myjetcaster/ui/home/discover/Discover.kt
+++ app/src/main/java/ru/aasmc/myjetcaster/ui/home/discover/Discover.kt
@@ -1,2 +1,112 @@
 package ru.aasmc.myjetcaster.ui.home.discover
 
+import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import ru.aasmc.myjetcaster.data.Category
+import ru.aasmc.myjetcaster.ui.home.category.PodcastCategory
+import ru.aasmc.myjetcaster.ui.theme.Keyline1
+
+
+@Composable
+fun Discover(
+    modifier: Modifier = Modifier
+) {
+    val viewModel: DiscoverViewModel = viewModel()
+    val viewState by viewModel.state.collectAsState()
+
+    val selectedCategory = viewState.selectedCategory
+    if (viewState.categories.isNotEmpty() && selectedCategory != null) {
+        Column(modifier) {
+            Spacer(modifier = Modifier.height(8.dp))
+            PodcastCategoryTabs(
+                categories = viewState.categories,
+                selectedCategory = selectedCategory,
+                onCategorySelected = viewModel::onCategorySelected,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            // allows to switch between two layouts with a crossfade animation.
+            // targetState - is a key representing your target layout state.
+            // Every time you change a key the animation will be triggered.
+            Crossfade(
+                targetState = selectedCategory,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) { category ->
+                // todo. need to think about how this will scroll within the outer VerticalScroller
+                PodcastCategory(
+                    categoryId = category.id,
+                    modifier = Modifier
+                        .fillMaxSize()
+                )
+            }
+        }
+    }
+    // todo implement empty state
+}
+
+private val emptyTabIndicator: @Composable (List<TabPosition>) -> Unit = {}
+
+@Composable
+private fun PodcastCategoryTabs(
+    categories: List<Category>,
+    selectedCategory: Category,
+    onCategorySelected: (Category) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val selectedIndex = categories.indexOfFirst { it == selectedCategory }
+    ScrollableTabRow(
+        selectedTabIndex = selectedIndex,
+        divider = {},
+        edgePadding = Keyline1,
+        indicator = emptyTabIndicator,
+        modifier = modifier
+    ) {
+        categories.forEachIndexed { index, category ->
+            Tab(
+                selected = index == selectedIndex,
+                onClick = { onCategorySelected(category) }
+            ) {
+                ChoiceChipContent(
+                    text = category.name,
+                    selected = index == selectedIndex,
+                    modifier = Modifier.padding(horizontal = 4.dp, vertical = 16.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun ChoiceChipContent(
+    text: String,
+    selected: Boolean,
+    modifier: Modifier = Modifier
+) {
+    Surface(
+        color = when {
+            selected -> MaterialTheme.colors.primary.copy(alpha = 0.08f)
+            else -> MaterialTheme.colors.onSurface.copy(alpha = 0.12f)
+        },
+        contentColor = when {
+            selected -> MaterialTheme.colors.primary
+            else -> MaterialTheme.colors.onSurface
+        },
+        shape = MaterialTheme.shapes.small,
+        modifier = modifier
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.body2,
+            modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+        )
+    }
+}
